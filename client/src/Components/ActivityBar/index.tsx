c88940af2c54bbf007849658cd7a382e2d0282bc
madhurgoyal19 <35370133+madhurgoyal19@users.noreply.github.com>
2023-08-09 12:57:21 +0530

    Create Activity Bar in Workspace Layout

---
+++ client/src/Components/ActivityBar/index.tsx
@@ -0,0 +1,9 @@
+const ActivityBar = () => {
+  return (
+    <div className="h-full min-w-16 bg-midnight-light border-r border-midnight-highlight">
+      <div className="border-b border-midnight-highlight h-14">Activity</div>
+    </div>
+  );
+};
+
+export default ActivityBar;
